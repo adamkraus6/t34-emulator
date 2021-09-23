825e43ee935f8f4b6937c0725f1b36d48f06bdaa
Adam Kraus <adam.kraus@mines.sdsmt.edu>
2021-09-23 10:17:03 -0600

    few changes, added comments

--- t34.py
+++ t34.py
@@ -18,11 +18,11 @@ def displayMem(loc):
     return
 
 def displayMemRange(start, end):
-    sections = list(chunks(memory[start:end+1], 8))
-    for section in sections:
+    chunked = list(chunks(memory[start:end+1], 8))
+    for chunk in chunked:
         print(format(start, "04X"), end=' ')
-        for no in section:
-            print(format(no, "02X"), end=' ')
+        for num in chunk:
+            print(format(num, "02X"), end=' ')
         start += 8
         print("")
     return
@@ -41,14 +41,17 @@ def runProg(loc):
 
 def main():
     if len(sys.argv) > 1:
+        # file argument supplied
         f = open(sys.argv[1], "r")
+        # read file lines
         lines = f .readlines()
         for line in lines:
             line = line[:-1]
             byteCount = int(line[1:3], 16)
             address = int(line[3:7], 16)
             recordType = int(line[7:9], 16)
-            if recordType != 1:
+            # if data record, edit memory with opcodes
+            if recordType == 0:
                 data = re.findall("..?", line[9:9+2*byteCount])
                 editMem(address, data)
     else:
@@ -58,18 +61,18 @@ def main():
     monitor = input("> ")
     while "exit" not in monitor:
         # check input
-        if "R" in monitor:
+        if "R" in monitor: # ex: 200R
             # run program at starting address
             runProg(int(monitor[:-1], 16))
-        elif "." in monitor:
+        elif "." in monitor: # ex: 200.20F
             # display memory range
             split = monitor.split(".")
             displayMemRange(int(split[0], 16), int(split[1], 16))
-        elif ":" in monitor:
+        elif ":" in monitor: # ex: 200: A9
             # edit memory locations
             split = monitor.split()
             editMem(int(split[0][:-1], 16), split[1:])    
-        else:
+        else: # ex: 200
             displayMem(int(monitor, 16))
 
         monitor = input("> ")
